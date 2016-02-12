3aea5674eace5672e3de0a8c5f071808c06bfba0
Mike Hatfield <oakraven13@gmail.com>
2016-02-11 20:34:26 -0400

    Completed module 5

--- app/app.js
+++ app/app.js
@@ -3,7 +3,27 @@
     
     angular
         .module("productManagement", 
-            ["common.services",
-             "productResourceMock"]);
+            [ "ui.router",
+              "common.services",
+              "productResourceMock"])
+        .config(["$stateProvider", "$urlRouterProvider", function($stateProvider,$urlRouterProvider){
+            $stateProvider
+                .state("home", {
+                    url: "/",
+                    templateUrl: "app/welcome-view.html"
+                })
+                .state("productList", {
+                    url: "/products",
+                    templateUrl: "app/products/product-list-view.html",
+                    controller: "ProductListController as vm"
+                })
+                .state("productEdit", {
+                    url: "/products/edit/:productId",
+                    templateUrl: "app/products/product-edit-view.html",
+                    controller: "ProductEditController as vm"
+                });
+                
+            $urlRouterProvider.otherwise("/");
+        }]);
     
 })();
